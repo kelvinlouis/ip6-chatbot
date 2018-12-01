c51f215853800964bebbbf096d2cf1662e5e7a0c
Nicola Cocquio <nicola.cocquio@students.fhnw.ch>
2018-12-01 17:08:44 +0100

    fix port

--- rasa-core-entrypoint.sh
+++ rasa-core-entrypoint.sh
@@ -10,7 +10,7 @@ envsubst < /app/credentials_template.yml > /app/credentials.yml
 
 echo "Waiting for rasa-nlu..."
 
-while ! nc -z rasa-nlu $NLU_PORT; do   
+while ! nc -z rasa-nlu 5000; do   
   sleep 0.1 # wait for 1/10 of the second before check again
 done
 
