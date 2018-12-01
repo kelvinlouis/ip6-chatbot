0ac334518646a859872d08ca0f5f78fb44252d54
Nicola Cocquio <nicola.cocquio@students.fhnw.ch>
2018-12-01 16:58:19 +0100

    start nlu with custom entrypoint script

--- rasa-core-entrypoint.sh
+++ rasa-core-entrypoint.sh
@@ -3,9 +3,17 @@
 set -e
 
 apt-get update
-apt-get install -y gettext-base
+apt-get install -y gettext-base netcat
 
 envsubst < /app/endpoints_template.yml > /app/endpoints.yml
 envsubst < /app/credentials_template.yml > /app/credentials.yml
+
+echo "Waiting for rasa-nlu..."
+
+while ! nc -z rasa-nlu $NLU_PORT; do   
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+
+echo "rasa-nlu launched"
 
 exec "$@" 
