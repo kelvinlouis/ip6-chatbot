ca216964f19536cfd89a1bfdb852b9a60f7a7fca
Nicola Cocquio <nicola.cocquio@students.fhnw.ch>
2018-12-02 12:33:24 +0100

    build custom docker images

--- rasa-core-entrypoint.sh
+++ rasa-core-entrypoint.sh
@@ -2,20 +2,7 @@
 
 set -e
 
-apt-get update
-apt-get install -y gettext-base netcat
-
-pip install -U "/app/pip-cache/tensorflow-1.10.0-cp36-cp36m-linux_x86_64.whl-avx2.whl"
-
 envsubst < /app/endpoints_template.yml > /app/endpoints.yml
 envsubst < /app/credentials_template.yml > /app/credentials.yml
-
-echo "Waiting for rasa-nlu..."
-
-while ! nc -z rasa-nlu 5000; do   
-  sleep 0.5 # wait for 1/10 of the second before check again
-done
-
-echo "rasa-nlu launched"
 
 exec "$@" 
--- rasa-nlu-entrypoint.sh
+++ rasa-nlu-entrypoint.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-apt-get update
-apt-get install -y gettext-base
-
-pip install -U "/app/pip-cache/tensorflow-1.10.0-cp36-cp36m-linux_x86_64.whl-avx2.whl"
-
 if [ -f /app/config_nlu_template.yml ]; then
   envsubst < /app/config_nlu_template.yml > /app/config_nlu.yml
 fi
