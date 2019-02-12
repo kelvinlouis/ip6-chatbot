a118fd63d5ab5444b5df007e382b8fea2c1c593f
ncocquio <nicola.cocquio@students.fhnw.ch>
2019-02-12 14:10:11 +0100

    Add nlu-evaluation scripts and data (#7)
    * Add nlu-evaluation scripts and data
    * Add README

---
+++ nlu-evaluation/nlu_analysers/dialogflow_analyser.py
@@ -0,0 +1,96 @@
+from analyser import *
+
+class DialogflowAnalyser(Analyser):
+	def __init__(self, api_key):
+		super(DialogflowAnalyser, self).__init__()
+		self.api_key = api_key
+		self.url = "https://api.api.ai/v1/query?v=20170101&sessionId=1234&lang=en&query=%s"
+	
+	def get_annotations(self, corpus, output):
+		data = json.load(open(corpus))		
+		annotations = {'results':[]}
+		
+		for s in data["sentences"]:
+			if not s["training"]: #only use test data
+				encoded_text = urllib.quote(s['text'])
+				headers = { 'Authorization' : 'Bearer %s' %  self.api_key}
+				annotations['results'].append(requests.get(self.url % encoded_text,data={},headers=headers).json())
+		
+		file = open(output, "w")
+  		file.write(json.dumps(annotations, sort_keys=False, indent=4, separators=(',', ': '), ensure_ascii=False).encode('utf-8'))
+  		file.close()   
+  		
+  	def analyse_annotations(self, annotations_file, corpus_file, output_file):
+  		analysis = {"intents":{}, "entities":{}}  		
+  		
+  		corpus = json.load(open(corpus_file))
+  		gold_standard = []
+  		for s in corpus["sentences"]:
+			if not s["training"]: #only use test data
+				gold_standard.append(s)
+  		
+  		annotations = json.load(open(annotations_file))
+  		
+  		i = 0
+  		for a in annotations["results"]:
+  			if not a["result"]["resolvedQuery"] == gold_standard[i]["text"]:
+  				print "WARNING! Texts not equal"
+  			 
+  			#intent  			 			
+  			try:
+  				aIntent = a["result"]["metadata"]["intentName"]
+  			except:
+  				aIntent = "None"  				
+  			oIntent = gold_standard[i]["intent"]
+  			
+  			Analyser.check_key(analysis["intents"], aIntent)
+  			Analyser.check_key(analysis["intents"], oIntent)
+  			
+  			if aIntent == oIntent:
+  				#correct
+  				analysis["intents"][aIntent]["truePos"] += 1
+  			else:
+  				#incorrect
+  				analysis["intents"][aIntent]["falsePos"] += 1
+  				analysis["intents"][oIntent]["falseNeg"] += 1
+  				
+  				
+  			#entities
+  			try:
+  				aEntities = a["result"]["parameters"]
+  			except:
+  				aEntities=[]
+  			oEntities = gold_standard[i]["entities"]
+  			  			  			
+  			for x in aEntities:
+  				Analyser.check_key(analysis["entities"], x)
+  				
+  				if len(oEntities) < 1: #false pos
+  					analysis["entities"][x]["falsePos"] += 1
+  				else:
+  					truePos = False
+  					
+  					for y in oEntities:
+  						if aEntities[x].rstrip('.').lower() == y["text"].lower():
+  							if x == y["entity"]: #truePos
+  								truePos = True
+  								oEntities.remove(y)
+  								break
+  							else:						 #falsePos + falseNeg
+  								analysis["entities"][x]["falsePos"] += 1
+  								analysis["entities"][y["entity"]]["falseNeg"] += 1
+  								oEntities.remove(y)
+  								break
+  					if truePos:
+  						analysis["entities"][x]["truePos"] += 1
+  					else:
+  						analysis["entities"][x]["falsePos"] += 1	
+  				
+  				
+  			for y in oEntities:
+  				Analyser.check_key(analysis["entities"], y["entity"])
+  				analysis["entities"][y["entity"]]["falseNeg"] += 1	  					
+  			  			  				
+  			i += 1	
+  		
+  		self.write_json(output_file, json.dumps(analysis, sort_keys=False, indent=4, separators=(',', ': '), ensure_ascii=False).encode('utf-8'))	
